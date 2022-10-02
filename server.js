6da65ed0fb382afc901515ca053bf7a77e3b48ae
Cymmer John Maranga <cymmerjohnmaranga@Cymmers-MacBook-Pro.local>
2022-10-02 22:06:22 +0800

    Adds default route for checking

--- server.js
+++ server.js
@@ -1,23 +1,27 @@
 const { execSync } = require("child_process");
-const express = require('express')
-const app = express()
-const port = 5050
+const express = require("express");
+const app = express();
+const port = 5050;
 
-app.post('/install-clang', (req, res) => {
-	execSync("apt -o DPkg::Options::=--force-confdef install clang -y");
-  res.json({ status: "finished"})
-})
+app.get("/", (req, res) => {
+  res.json({ status: "running" });
+});
 
-app.post('/install-jdk', (req, res) => {
-	execSync("apt -o DPkg::Options::=--force-confdef install openjdk-17 -y");
-  res.json({ status: "finished"})
-})
+app.post("/install-clang", (req, res) => {
+  execSync("apt -o DPkg::Options::=--force-confdef install clang -y");
+  res.json({ status: "finished" });
+});
 
-app.post('/install-csharp', (req, res) => {
-	execSync("apt -o DPkg::Options::=--force-confdef install mono -y");
-  res.json({ status: "finished"})
-})
+app.post("/install-jdk", (req, res) => {
+  execSync("apt -o DPkg::Options::=--force-confdef install openjdk-17 -y");
+  res.json({ status: "finished" });
+});
+
+app.post("/install-csharp", (req, res) => {
+  execSync("apt -o DPkg::Options::=--force-confdef install mono -y");
+  res.json({ status: "finished" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
