1693c8e36db061b6c46e3c78b3fd3c417bf13250
Cymmer John Maranga <cymmerjohnmaranga@Cymmers-MacBook-Pro.local>
2022-10-02 22:21:29 +0800

    Adds /check-csharp

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const { execSync } = require("child_process");
+const { execSync, exec } = require("child_process");
 const express = require("express");
 const app = express();
 const port = 5050;
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.json({ status: "running" });
 });
 
+/**
+ *
+ * C
+ *
+ */
 app.get("/check-clang", (req, res) => {
   const result = execSync("which clang");
   if (result !== "") {
@@ -21,11 +26,31 @@ app.post("/install-clang", (req, res) => {
   return res.json({ status: "finished" });
 });
 
+/**
+ *
+ *
+ * Java
+ *
+ */
 app.post("/install-jdk", (req, res) => {
   execSync("apt -o DPkg::Options::=--force-confdef install openjdk-17 -y");
   return res.json({ status: "finished" });
 });
 
+/**
+ *
+ *
+ * C#
+ *
+ */
+app.get("/check-csharp", (req, res) => {
+  const result = execSync("which mono");
+  if (result !== "") {
+    return res.json({ status: "installed" });
+  }
+
+  return res.json({ status: "not-installed" });
+});
 app.post("/install-csharp", (req, res) => {
   execSync("apt -o DPkg::Options::=--force-confdef install mono -y");
   return res.json({ status: "finished" });
