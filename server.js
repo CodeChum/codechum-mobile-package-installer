06c56459be4fbe0495220d4bc4d04351390e723f
Cymmer John Maranga <cymmerjohnmaranga@Cymmers-MacBook-Pro.local>
2022-10-02 21:32:37 +0800

    Uses express to have post routes

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
-const http = require("http");
 const { execSync } = require("child_process");
+const express = require('express')
+const app = express()
+const port = 5050
 
-const hostname = "127.0.0.1";
-const port = 5050;
+app.post('/install-clang', (req, res) => {
+	execSync("apt -o DPkg::Options::=--force-confdef install clang -y");
+  res.json({ status: "finished"})
+})
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "application/json");
+app.post('/install-jdk', (req, res) => {
+	execSync("apt -o DPkg::Options::=--force-confdef install openjdk-17 -y");
+  res.json({ status: "finished"})
+})
 
-  switch (req.url) {
-    case "/install-clang":
-      execSync("apt -o DPkg::Options::=--force-confdef install clang -y");
-      res.end(JSON.stringify({ status: "finished" }));
-      break;
-    case "/install-java":
-      execSync("apt -o DPkg::Options::=--force-confdef install openjdk-17 -y");
-			res.end(JSON.stringify({ status: "finished" }));
-      break;
-    case "/install-csharp":
-      execSync("apt -o DPkg::Options::=--force-confdef install mono -y");
-			res.end(JSON.stringify({ status: "finished" }));
-      break;
-    default:
-      break;
-  }
-});
+app.post('/install-csharp', (req, res) => {
+	execSync("apt -o DPkg::Options::=--force-confdef install mono -y");
+  res.json({ status: "finished"})
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
