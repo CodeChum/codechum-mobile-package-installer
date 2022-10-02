5ebadafbb0ba6048304fcaf0b49700e05818dd69
Cymmer John Maranga <cymmerjohnmaranga@Cymmers-MacBook-Pro.local>
2022-10-02 22:17:44 +0800

    Adds /check-clang endpoint

--- server.js
+++ server.js
@@ -7,21 +7,30 @@ app.get("/", (req, res) => {
   res.json({ status: "running" });
 });
 
+app.get("/check-clang", (req, res) => {
+  const result = execSync("which clang");
+  if (result !== "") {
+    return res.json({ status: "installed" });
+  }
+
+  return res.json({ status: "not-installed" });
+});
+
 app.post("/install-clang", (req, res) => {
   execSync("apt -o DPkg::Options::=--force-confdef install clang -y");
-  res.json({ status: "finished" });
+  return res.json({ status: "finished" });
 });
 
 app.post("/install-jdk", (req, res) => {
   execSync("apt -o DPkg::Options::=--force-confdef install openjdk-17 -y");
-  res.json({ status: "finished" });
+  return res.json({ status: "finished" });
 });
 
 app.post("/install-csharp", (req, res) => {
   execSync("apt -o DPkg::Options::=--force-confdef install mono -y");
-  res.json({ status: "finished" });
+  return res.json({ status: "finished" });
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Package Installer listening on port ${port}`);
 });
